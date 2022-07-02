46ad5bfc1ed5e39efdea3fc501887378e65a15d0
gimo <goodmoskito@gmail.com>
2022-07-02 14:13:18 +0900

    apis 개선

--- server/apis.py
+++ server/apis.py
@@ -28,20 +28,46 @@ class KiwoomApiService(object):
         except:
             return dict()
 
+    def get_stocks_daily_candle_not_entered(self, data_set, date):
+        for n, data in enumerate(data_set):
+            if date in data:
+                # 오늘 제외, 전날부터 미입력 값 넣어야 함.
+                # 최대 600일전까지 나오는데, 그정도로 값이 갱신이 안되진 않을 것이라고 판단함.
+                return data_set[:n]
+
+    def get_all_daily_candle_by_stock_code(self, stock_code, latest_date):
+        is_repeat = IsRepeat.NO
+        total = []
+        while True:
+            result = self.set_and_get_result(
+                Commands.GET_ALL_DAILY_CANDLE,
+                code=stock_code,
+                latest_date=latest_date,
+                is_repeat=is_repeat
+            )
+
+            input_date = GetQueries.is_exist_table_by_stock_code(stock_code)  # timestamp
+            if input_date:
+                not_entered = self.get_stocks_daily_candle_not_entered(result['total'], input_date[0][0])
+                if not_entered:
+                    total += not_entered
+                return total
+
+            # 반복시 마지막 날짜를 기준으로 가져오므로
+            if is_repeat == IsRepeat.YES:
+                # 반복 상태가 아닌 경우 수집 종료.
+                total += result['total']
+                return total
+
+            total += result['total'][:-1]
+
     def get_history_price_all_stocks(self):
         codes = self.set_and_get_result(Commands.GET_ALL_KOREAN_STOCK_CODE)
 
         latest_date = (datetime.datetime.now() - datetime.timedelta(days=1))
+        result = dict()
         for code in codes:
-            result = self.set_and_get_result(
-                Commands.GET_ALL_DAILY_CANDLE,
-                code=code,
-                latest_date=latest_date,
-                is_repeat=IsRepeat.NO
-            )
+            total_result = self.get_all_daily_candle_by_stock_code(code, latest_date)
+            result[code] = total_result
 
-            if result['repeat'] == IsRepeat.YES:
-                pass
-
-    def get_all_stocks_korean_name(self):
-        pass
+        return result
