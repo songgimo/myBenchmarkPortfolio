c0c502541b96c6ccf4edd77e92fcd1dd7f48e6a4
gimo <goodmoskito@gmail.com>
2022-07-24 14:19:00 +0900

    app&apis.py fixed

--- server/apis.py
+++ server/apis.py
@@ -1,27 +1,64 @@
+import yfinance
+import datetime
+import json
+from flask import request
 from flask_restful import Resource
-from flask import jsonify, request
 
-from server.queries import GetQueries
+# 내 주식을 입력 -> 차트에 노출됨 ->
+# 벤치마크 주식을 입력 -> 차트에 노출됨
+# 괴리감 계산
 
 
-class GetDailyCandle(Resource):
+class Result(object):
+    def __init__(self, success, data, message):
+        self.__success = success
+        self.__data = data
+        self.__message = message
+
+    def to_dict(self):
+        return {
+            "success": self.__success,
+            "data": self.__data,
+            "message": self.__message
+        }
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+
+class GetChart(Resource):
     def get(self):
         args = request.args
-        stock_code = args.get('stock_code')
+        print(list(args))
+        code_name = args.get("code")
+        print(code_name)
+        last_three_years = datetime.datetime.now() - datetime.timedelta(days=365*3)
+        yf = yfinance.download(code_name, last_three_years.strftime("%Y-%m-%d"))
 
-        candles = GetQueries.all_candle_by_name(stock_code)
+        axes_for_timestamp = [axes.timestamp() * 1000 for axes in yf.axes[0]]
+        zip_for_close = list(zip(axes_for_timestamp, list(yf.Close)))
 
-        if candles:
-            return {
-                "success": False,
-                "data": {
-                    "candles": candles
-                },
-                "message": ''
-            }
-        else:
-            return {
-                "success": False,
-                "data": '',
-                "message": ''
-            }
+        return Result(True, zip_for_close, None).to_dict()
+
+#
+# class GetDailyCandle(Resource):
+#     def get(self):
+#         args = request.args
+#         stock_code = args.get('stock_code')
+#
+#         candles = GetQueries.all_candle_by_name(stock_code)
+#
+#         if candles:
+#             return {
+#                 "success": False,
+#                 "data": {
+#                     "candles": candles
+#                 },
+#                 "message": ''
+#             }
+#         else:
+#             return {
+#                 "success": False,
+#                 "data": '',
+#                 "message": ''
+#             }
--- server/apps.py
+++ server/apps.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 
-from server.apis import GetDailyCandle
-
+from server.apis import GetChart
 
 app = Flask(__name__)
 api = Api()
@@ -11,7 +10,7 @@ api = Api()
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-api.add_resource(GetDailyCandle, '/api/v0/get/daily-candle')
+api.add_resource(GetChart, '/api/v0/get/last-three-years-chart')
 api.init_app(app)
 
 
